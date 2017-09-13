1b8fd9851c15fd597cc1733b4b7ea86e5ae32fcc
Timothy Luciani <onorinbejasus@gmail.com>
2017-09-12 23:04:01 -0500

    -- Added a start/stop rendering for particles on the floor

--- Assignment1/src/js/utilities/utilities.js
+++ Assignment1/src/js/utilities/utilities.js
@@ -7,7 +7,7 @@ Assignment 1
 "use strict";
 const Utilities = function(){
 
-  const FLOOR_DAMPENING  = 0.7;
+  const FLOOR_RESTITUTION  = 0.7;
   let epsilon = 0.01;
 
   return {
@@ -60,6 +60,11 @@ const Utilities = function(){
           return c;
         },
 
+        /* Get the magnitude of the vector */
+        magnitude: function(a) {
+          return Math.sqrt(Utilities.Vector_Utils.dot(a,a));
+        },
+
         /* Zero out all values */
         zero: function(a) {
           let c = {};
@@ -92,21 +97,35 @@ const Utilities = function(){
                 Utilities.Vector_Utils.multiply_components(o.normal, {x:1, y:Y_UP})),
 
               v = Utilities.Vector_Utils.dot(
-                particle.velocity,
-                Utilities.Vector_Utils.multiply_components(o.normal, {x:1, y:Y_UP}));
+                particle.velocity, Utilities.Vector_Utils.multiply_components(o.normal, {x:1, y:Y_UP})
+              );
 
               /* Check if a collision occurred */
               if(c < e && v < 0) {
-
                 particle.collision = true;
-                particle.deformation = particle.radius - c + 0.1;
+                particle.deformation = particle.radius - c;
                 // particle.motion = false;
+
+                let absorbed_force = (1.0-FLOOR_RESTITUTION) * particle.velocity.y;
 
                 if(o.type === "wall") {
                   /* Flip the tangent component on the velocity */
-                  particle.velocity.y = -FLOOR_DAMPENING * particle.velocity.y;
+                  particle.velocity.y = Y_UP * FLOOR_RESTITUTION * particle.velocity.y;
                   /* place the particle one radius above the object */
                   particle.position.y = o.position.y - particle.radius;
+                }
+              }
+
+              /* Stop the particle if it is just resting on the bottom */
+              if(o.name === "floor"){
+                if(Math.abs(Utilities.Vector_Utils.magnitude(particle.forces) - 9.81) < 0.1 &&
+                    Math.abs(o.position.y - particle.position.y) <= particle.radius
+                ){
+                  particle.motion = false;
+                  console.log("stop");
+                }
+                else {
+                  particle.motion = true;
                 }
               }
 
@@ -114,7 +133,6 @@ const Utilities = function(){
           }
         }
     }()
-
   };
 
 }();
