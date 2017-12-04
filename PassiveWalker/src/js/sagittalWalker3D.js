ff575b72217370fefe67e96db9576267ddcd2ae4
onorinbejasus <tluciani21@gmail.com>
2017-12-04 14:50:43 -0600

    -- Finished the collision detectiion of the walker

--- PassiveWalker/src/js/sagittalWalker3D.js
+++ PassiveWalker/src/js/sagittalWalker3D.js
@@ -33,7 +33,7 @@ let Sagittal_Walker_3D = (function() {
         steps = 0,
         collision_found = false,
         last_collision_t = 0,
-        step_period = 0.035;
+        step_period = -1;
 
     function passive_motion_ODE45(y){
       let solution = y;
@@ -48,7 +48,7 @@ let Sagittal_Walker_3D = (function() {
           H22 = I + m*(b-d)*(b-d),
           H = math.matrix([[H11,H12],[H12,H22]]),
           det_H = math.det(H),
-          H_inv = math.matrix([[H22/det_H,-H12/det_H],[-H12/det_H,H11/det_H]]);//math.multiply(H, 1.0/det_H);
+          H_inv = math.matrix([[H22/det_H,-H12/det_H],[-H12/det_H,H11/det_H]]);
       let
           C11 = m*R*(b+d)*theta_st_p*Math.sin(theta_st-gamma)
               + 0.5*m*d*(b-d) * theta_sw_p*Math.sin(theta_st-theta_sw),
@@ -83,35 +83,61 @@ let Sagittal_Walker_3D = (function() {
       // dydt[3] = theta_dp._data[1]* 1; dydt[3] = +dydt[3].toFixed(5);
     }
 
-    /* Transitions from pre collision conditions to post */
-    /* Equations:[theta_p,  =  [ -1   0                    0   0,   x  [theta_m,
-*             theta_pp,       0   cos(2 x theta_m)     0   0,       theta_mp,
-*             phi_p,         -2   0                    0   0,       phi_m,
-*             phi_pp ]        0   cos(theta_pp) x      0   0 ]      phi_mp ]
-*                                   (1-cos(theta_pp)                      */
     function Poincare_map(y){
+
+      let Omega_m = [
+        2.0*b*d*Math.cos(y[1]-y[0])-(b+d)*R*Math.cos(y[1]-gamma) -
+          2.0*b*R*Math.cos(y[0]-gamma) + 2.0*R*R + b*b - b*d,
+        (b-d)*(b-R*Math.cos(y[1]-gamma)),
+        (b-d)*(b-R*Math.cos(y[1]-gamma)),
+        0
+      ],
+        oM = math.matrix([
+          [Omega_m[0],Omega_m[1]],
+          [Omega_m[2],Omega_m[3]]
+        ]);
+
+      let Omega_p = [
+        (b-d)*(d*Math.cos(y[0]-y[1]) - R*Math.cos(y[0]-gamma)+(b-d)),
+        -R*(b-d)*Math.cos(y[0]-gamma) - R*(b+2.0*d) * Math.cos(y[1]-gamma) + d*d + 2.0*R*R +
+          R*b*Math.cos(y[1]+gamma) - b*b*Math.cos(2.0*y[1]) + d*(b-d)*Math.cos(y[0]-y[1]),
+        (b-d)*(b-d),
+        (b-d)*(d*Math.cos(y[0]-y[1]) - R*Math.cos(y[0]-gamma))
+      ],
+        oP = math.matrix([
+          [Omega_p[0],Omega_p[1] ],
+          [Omega_p[2],Omega_p[3]]
+        ]),
+
+        det_oP = math.det(oP),
+
+        oP_inv = math.matrix([
+          [ Omega_p[3]/det_oP,-Omega_p[1]/det_oP],
+          [-Omega_p[2]/det_oP,Omega_p[0]/det_oP]
+        ]);
+
+      let Omega_p_m = math.multiply(oP_inv, oM),
+          ydot = math.matrix([ [y[2]], [y[3]] ]),
+          Poincare = math.multiply(Omega_p_m, ydot);
+
+      return [Poincare._data[0][0], Poincare._data[1][0]];
     }
 
     /* A collision occurs when: phi - 2*theta = 0*/
     function collision_check(theta, phi) {
-      let collision = Math.abs(phi) - Math.abs(2.0 * theta);
+      let collision = Math.abs(+theta.toFixed(4)) - Math.abs(2.0 * +phi.toFixed(4));
+          collision = +(collision.toFixed(4));
 
-      console.log(theta, phi, collision);
-
-      if(Math.abs(+collision.toFixed(5)) === 0) {
-        return true;
-      }
-      return false;
+      return !Math.abs(collision);
     }
 
     function update_walker(theta, phi){
+
     }
 
     function walk(time) {
 
-      let t = [], y = [], target_time = time + step_size;
-
-      console.log(time);
+      let t = [], y = [], target_time = time + step_size, omega;
 
       while( internal_time <= target_time ) {
 
@@ -119,23 +145,45 @@ let Sagittal_Walker_3D = (function() {
 
         y.push(_.clone(dydt));
 
-        console.log(internal_time);
+        // console.log(internal_time);
 
         /* On collision, apply the Poincare map and update the walker */
-        if( Math.abs(+dydt[0].toFixed(3)) > 0 && collision_check(dydt[0], dydt[1])) {
-          /* Apply the Poincare map and update the solver's value*/
-          //Solver.y = Poincare_map( _.clone(Solver.y) );
-          /* Mark that a collision was found */
-          collision_found = !collision_found;
+        if(step_period < 0){ // no gait determined yet, use the angles
+          if( Math.abs(+dydt[0].toFixed(3)) > 0 && collision_check(dydt[0], dydt[1])) {
+            /* Apply the Poincare map and update the solver's value*/
+            omega = Poincare_map(dydt);
+            /* Mark that a collision was found */
+            collision_found = true;
+            last_collision_t = step_period = internal_time;
+          }
+        }
+        else if((internal_time - last_collision_t) > step_period ){
+          omega = Poincare_map(dydt);
+          collision_found = true;
+          last_collision_t = internal_time;
+        }
+
+        /* If the collision is found, calculate the impulse's effect on the other foot */
+        if(collision_found){
+          let previous = y.pop();
+
+          /* Reverse the leg angles based on the impulse */
+          dydt[0] = previous[1];
+          dydt[1] = previous[0];
+          dydt[2] = omega[1];
+          dydt[3] = omega[0];
+
+          /* Push the new solution onto the list*/
+          y.push(dydt);
+
+          collision_found = false;
+          console.log(dydt);
         }
 
 
         internal_time += step_size;
         steps++;
 
-        // // Store the solution at this time step:
-        // t.push( Solver.t );
-        // y.push( _.clone(Solver.y) );
       }
 
       // let current = y.slice(-1)[0];
